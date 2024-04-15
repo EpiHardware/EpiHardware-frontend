9697e6a44fa98ed643f171ad47f151b633385b98
Antoine Labard <antoine@labard.fr>
2024-04-15 15:18:40 +0200

    add all the methods prototypes for OrderMethods.ts

--- src/data/methods/OrderMethods.ts
+++ src/data/methods/OrderMethods.ts
@@ -1,3 +1,18 @@
+import Order from "../models/Order";
+import User from "../models/User";
+
 abstract class OrderMethods {
+    public static async getAll(user: User, token: string): Promise<Order[] | Error> {
+        //Todo Implement this method
+    }
+
+    public static async get(order: Order, user: User, token: string): Promise<Order | Error> {
+        //todo Implement this method
+    }
+
+    public static async checkout(order: Order, user: User, token: string): Promise<Order | Error> {
+        //Todo: Implement this method
+    }
 }
+
 export default OrderMethods
