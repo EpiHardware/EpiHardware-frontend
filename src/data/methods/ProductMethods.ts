17a8a3241daef27d352ceaaaa32b12f36a65f14f
Antoine Labard <antoine@labard.fr>
2024-04-15 12:18:48 +0200

    add an example function prototype and implementation

--- src/data/methods/ProductMethods.ts
+++ src/data/methods/ProductMethods.ts
@@ -1,3 +1,25 @@
+import Product from "../models/Product";
+
 abstract class ProductMethods {
+    public static async getAll(): Promise<Product[] | Error> {
+        return await fetch('http://localhost:3000/products') // Todo: update the URL of this fetch request
+            .then(response => {
+                let data: any = response.json()
+                if (!response.ok) {
+                    throw new Error(data.message)
+                }
+                return data
+            })
+            .then(data => {
+                return data.map((productData: any) => {
+                    return new Product(
+                        productData.id, productData.name, productData.description, productData.price, productData.photo)
+                })
+            })
+            .catch((error) => {
+                return error
+            })
+    }
 }
+
 export default ProductMethods
