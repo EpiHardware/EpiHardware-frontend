31ebae1bcadff10d41293afbeb36bc3a8dfdd604
Antoine Labard <antoine@labard.fr>
2024-04-15 14:39:01 +0200

    add all the methods prototypes for UserMethods.ts

--- src/data/methods/UserMethods.ts
+++ src/data/methods/UserMethods.ts
@@ -1,3 +1,25 @@
+import User from "../models/User";
+
 abstract class UserMethods {
+
+    public static async register(
+        login: string,
+        email: string,
+        firstname: string,
+        lastname: string,
+        password: string,
+    ): Promise<void|Error> {
+        //Todo: Implement the register method
+    }
+
+    public static async login(login: string, password: string): Promise<string|Error> {
+        //Todo: Implement the login method
+    }
+
+    public static async update(user: User, token: string): Promise<void|Error> {
+        //Todo: Implement the update method
+    }
+
+    public static async get(login: string, token: string): Promise<User|Error> {}
 }
 export default UserMethods
