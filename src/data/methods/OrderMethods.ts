cbf2fee8cf68301eb0dc620302957d5c2570fc28
Antoine Labard <antoine@labard.fr>
2024-04-16 15:09:29 +0200

    implement order methods for OrderMethods.ts

--- src/data/methods/OrderMethods.ts
+++ src/data/methods/OrderMethods.ts
@@ -3,18 +3,63 @@ import Product from "../models/Product";
 
 abstract class OrderMethods {
     public static async getAll(): Promise<Order[] | Error> {
-        return Error("Not Implemented")
-        //Todo Implement this method
+        return await fetch('http://localhost:5432/orders')
+            .then(response => {
+                let data: any = response.json()
+                if (!response.ok) {
+                    throw new Error(data.message)
+                }
+                return data
+            })
+            .then(data => {
+                return data.map((orderData: any) => {
+                    let products = orderData.products.map((productData: any) => {
+                        return new Product(
+                            productData.id,
+                            productData.name,
+                            productData.description,
+                            productData.price,
+                            productData.photo)
+                    })
+                    return new Order(orderData.id, orderData.totalPrice, orderData.creationDate, products)
+                })
+            })
+            .catch((error) => error)
     }
 
     public static async get(id: string): Promise<Order | Error> {
-        return Error("Not Implemented")
-        //todo Implement this method
+        return await fetch(`http://localhost:5432/orders/${id}`)
+            .then(response => {
+                let data: any = response.json()
+                if (!response.ok) {
+                    throw new Error(data.message)
+                }
+                return data;
+            })
+            .then(orderData => {
+                let products = orderData.products.map((productData: any) => {
+                    return new Product(
+                        productData.id,
+                        productData.name,
+                        productData.description,
+                        productData.price,
+                        productData.photo)
+                })
+                return new Order(orderData.id, orderData.totalPrice, orderData.creationDate, products)
+            })
+            .catch((error) => {
+                return error
+            });
     }
 
-    public static async checkout(order: Order): Promise<Order | Error> {
-        return Error("Not Implemented")
-        //Todo: Implement this method
+    public static async checkout(order: Order): Promise<void> {
+        await fetch('http://localhost:5432/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(order.toJSON())
+        })
     }
 
     public static addToCart(product: Product): void {
@@ -29,7 +74,7 @@ abstract class OrderMethods {
         //todo Implement this method
     }
 
-    public static getCart(): Product[]|Error {
+    public static getCart(): Product[] | Error {
         return Error("Not Implemented")
         //todo Implement this method
     }
