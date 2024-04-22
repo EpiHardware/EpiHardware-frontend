481985331603f7cc40391c84efb8447ed1b731a4
Antoine Labard <antoine@labard.fr>
2024-04-22 11:04:29 +0200

    feat: implement methods for UserMethods.ts

--- src/data/methods/UserMethods.ts
+++ src/data/methods/UserMethods.ts
@@ -2,38 +2,95 @@ import User from "../models/User";
 
 abstract class UserMethods {
 
-    public static async register(
-        login: string,
-        email: string,
-        firstname: string,
-        lastname: string,
-        password: string,
-    ): Promise<void|Error> {
-        //Todo: Implement the register method
+    public static async register(user: User): Promise<void | Error> {
+        return await fetch('http://localhost:5432/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user.toJSON())
+        })
+            .then(response => {
+                let data: any = response.json()
+                if (!response.ok) {
+                    throw new Error(data.message)
+                }
+            })
+            .catch((error) => {
+                return error
+            });
     }
 
-    public static async login(login: string, password: string): Promise<string|Error> {
-        return Error("Not Implemented")
-        //Todo: Implement the login method
+    public static async login(login: string, password: string): Promise<void | Error> {
+        return await fetch('http://localhost:5432/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({login: login, password: password})
+        })
+            .then(response => {
+                let data: any = response.json()
+                if (!response.ok) {
+                    throw new Error(data.message)
+                }
+                this.addToken(data.token)
+            })
+            .catch((error) => {
+                return error
+            });
     }
 
-    public static async update(user: User, token: string): Promise<void|Error> {
-        return Error("Not Implemented")
-        //Todo: Implement the update method
+    public static async update(user: User): Promise<void | Error> {
+        return await fetch('http://localhost:5432/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({user: user.toJSON(), token: this.getToken()})
+        })
+            .then(response => {
+                let data: any = response.json()
+                if (!response.ok) {
+                    throw new Error(data.message)
+                }
+            })
+            .catch((error) => {
+                return error
+            });
     }
 
-    public static async get(login: string, token: string): Promise<User|Error> {
-        return Error("Not Implemented")
-        //todo Implement this method
+    public static async getDetails(login: string, token: string): Promise<User | Error> {
+        return await fetch('http://localhost:5432/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({login: login, token: token})
+        })
+            .then(response => {
+                let data: any = response.json()
+                if (!response.ok) {
+                    throw new Error(data.message)
+                }
+                return User.fromJSON(data.user)
+            })
+            .catch((error) => {
+                return error
+            })
     }
 
-    public static getToken(): string|null {
-        return null
-        //todo Implement this method
+    public static getToken(): string | null {
+        return localStorage.getItem("token")
     }
 
     public static addToken(token: string): void {
-        //todo Implement this method
+        localStorage.setItem("token", token)
+    }
+
+    public static removeToken(token: string): void {
+        localStorage.removeItem("token")
     }
 }
+
 export default UserMethods
