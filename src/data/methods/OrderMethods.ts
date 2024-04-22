dcd4910ea7db0483ae840d5de750f6cb26c22d3c
Antoine Labard <antoine@labard.fr>
2024-04-22 11:36:01 +0200

    fix: reimplement the cart methods

--- src/data/methods/OrderMethods.ts
+++ src/data/methods/OrderMethods.ts
@@ -1,5 +1,6 @@
 import Order from "../models/Order";
 import Product from "../models/Product";
+import UserMethods from "./UserMethods";
 
 abstract class OrderMethods {
     public static async getAll(): Promise<Order[] | Error> {
@@ -53,49 +54,76 @@ abstract class OrderMethods {
     }
 
     public static async checkout(order: Order): Promise<void> {
-        await fetch('http://localhost:5432/api/orders', {
+        return await fetch('http://localhost:5432/api/orders', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(order.toJSON())
-        })
-    }
-
-    public static addToCart(product: Product): void {
-        let cart = OrderMethods.getCart()
-        cart.push(product)
-        localStorage.setItem("cart", JSON.stringify(cart))
-    }
-
-    public static removeFromCart(product: Product): void {
-        let cart = this.getCart()
-        for (let i = 0; i < cart.length; ++i) {
-            if (cart[i].id === product.id) {
-                cart.splice(i, 1)
-                break
+            body: JSON.stringify({order: order.toJSON(), token: UserMethods.getToken()})
+        }).then(response => {
+            let data: any = response.json()
+            if (!response.ok) {
+                throw new Error(data.message)
             }
-        }
-        localStorage.setItem("cart", JSON.stringify(cart))
-    }
-
-    public static clearCart(): void {
-        localStorage.setItem("cart", JSON.stringify([]))
-    }
-
-    public static getCart(): Product[] {
-        let cart = localStorage.getItem("cart")
-        if (cart === null) {
-            return []
-        }
-        return JSON.parse(cart).map((product: any) => {
-            return new Product(
-                product.id,
-                product.name,
-                product.description,
-                product.price,
-                product.photo);
         })
+            .catch((error) => {
+                return error
+            });
+    }
+
+    public static async addToCart(product: Product): Promise<void | Error> {
+        return await fetch(`http://localhost:5432/api/carts/${product.id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({token: UserMethods.getToken()})
+        }).then(response => {
+            let data: any = response.json()
+            if (!response.ok) {
+                throw new Error(data.message)
+            }
+        })
+            .catch((error) => {
+                return error
+            });
+    }
+
+    public static async removeFromCart(product: Product): Promise<void | Error> {
+        return await fetch(`http://localhost:5432/api/carts/${product.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({token: UserMethods.getToken()})
+        }).then(response => {
+            let data: any = response.json()
+            if (!response.ok) {
+                throw new Error(data.message)
+            }
+        })
+            .catch((error) => {
+                return error
+            });
+    }
+
+    public static async getCart(): Promise<Product[] | Error> {
+        return await fetch(`http://localhost:5432/api/carts/}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({token: UserMethods.getToken()})
+        }).then(response => {
+            let data: any = response.json()
+            if (!response.ok) {
+                throw new Error(data.message)
+            }
+            return data.map((productData: any) => {return Product.fromJSON(productData)})
+        })
+            .catch((error) => {
+                return error
+            });
     }
 }
 
