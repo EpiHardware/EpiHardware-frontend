6d344445ec32d1662a08a6a7df1ceb985c034b76
Fabien <fabien.le-fichoux@epitech.eu>
2024-04-23 15:50:30 +0200

    Compenent:Add banner for offer ( header)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 import Layout from "../compenents/Layout";
+import Banner from "../compenents/Banner";
 
 const Home: FC = () => {
     return (
         <>
             <Layout />
+            <Banner />
         </>
     );
 };
